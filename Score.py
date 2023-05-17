50cb5a581a5fc359e9c905d3b8ae84edd27e69ce
bsensix <109923359+bsensix@users.noreply.github.com>
2023-05-17 15:19:28 -0300

    Score.py

--- Score.py
+++ Score.py
@@ -26,7 +26,7 @@ from PIL import Image
 
 #Barra Lateral
 barra_lateral = st.sidebar.empty()
-image = Image.open(r'C:\Users\breno\Desktop\TESTE_SRICPT\SCRIPT_CURVAS_FENOLOGICAS\Logo-Escuro.png')
+image = Image.open('Logo-Escuro.png')
 st.sidebar.image(image)
 st.sidebar.markdown('### Calculo Score Sentinel-2')
 # Upload Arquivo csv 
