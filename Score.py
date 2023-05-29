5594a13b19af6cc4f6e151e7cdad1ee6c24713b7
bsensix <109923359+bsensix@users.noreply.github.com>
2023-05-29 10:13:34 -0300

    Score.py

--- Score.py
+++ Score.py
@@ -62,25 +62,8 @@ print(tabela)
 
 
 # DataFrame para Planilha Excel em xlsx
+tabela_csv = tabela.to_csv(index=False).encode('utf-8')
 
-def to_excel(tabela):
-    output = BytesIO()
-    writer = pd.ExcelWriter(output, engine='xlsxwriter')
-    df.to_excel(writer, sheet_name='Sheet1')
-    writer.save()
-    processed_data = output.getvalue()
-    return processed_data
-
-def get_table_download_link(tabela):
-    """Generates a link allowing the data in a given panda dataframe to be downloaded
-    in:  dataframe
-    out: href string
-    """
-    val = to_excel(tabela)
-    b64 = base64.b64encode(val)  # val looks like b'...'
-    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="extract.xlsx">Download csv file</a>' # decode b'abc' => abc
-
-df = tabela
-st.markdown(get_table_download_link(df), unsafe_allow_html=True)
+st.download_button(label=' ⬇️ Download Levantamento Solos', data = tabela_csv ,file_name= 'Planilha_IVs.csv')
 
 
